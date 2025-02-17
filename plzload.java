3e4e2492d9744d140bfa95ff5ecc14a94269f4fc
Jennifer D <[email]jennifer.duan2@student.tdsb.on.ca>
2025-02-17 14:26:24 -0500

    renamed the files based on their content

--- plzload.java
+++ plzload.java
@@ -1,5 +1,37 @@
+import java.lang.Math;
+import java.util.*;
+
 public class plzload {
-    public static void main(String[] args) {
-        System.out.println("i wish my wifi liked me right now...");
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    
+    double a,b,c, discriminant, x1,x2;
+    System.out.print("Please enter the value for a: ");
+    a = input.nextDouble();
+    System.out.print("Please enter the value for b: ");
+    b = input.nextDouble();
+    System.out.print("Please enter the value for c: ");
+    c = input.nextDouble();
+
+    discriminant = ((Math.sqrt(b*b - 4*a*c))*100)/100;
+    System.out.println(discriminant);
+
+    if (discriminant > 0){
+
+      x1 = Math.round(((b*-1)+discriminant)/2*a)*100/100;
+      x2 = Math.round(((b*-1)-discriminant)/2*a)*100/100;
+      System.out.println("x1 = "+x1 + " x2 = "+x2);
+      
+    } else if (discriminant == 0){
+
+      x1 = Math.round(((b*-1)+discriminant)/2*a)*100/100;
+      x2 = Math.round(((b*-1)-discriminant)/2*a)*100/100;
+      System.out.println("Both solutions for this equation are "+x1); 
+      
+    } else {
+      System.out.println("There are no solutions for this quadratic eqn.");
     }
+    
+    
+  }
 }
